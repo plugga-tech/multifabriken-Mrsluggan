cef7c659457731bb5ccf8578766745113184d0dc
Mrsluggan <ericflyger@gmail.com>
2023-11-13 17:48:42 +0100

    Pipe class
    Pipe class complete

---
+++ Multifabriken/src/Pipe.java
@@ -0,0 +1,18 @@
+public class Pipe {
+
+    private double diameter;
+    private double length;
+
+
+    public Pipe(double diameter, double length){
+
+        this.diameter = diameter;
+        this.length = length;
+
+
+
+
+    }
+
+    
+}
