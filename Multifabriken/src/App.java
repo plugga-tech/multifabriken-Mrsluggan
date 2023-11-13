a4b5d1b6610c22407796feed0ffeac74f8c960b9
Mrsluggan <ericflyger@gmail.com>
2023-11-13 19:16:37 +0100

    added function "quit"

--- Multifabriken/src/App.java
+++ Multifabriken/src/App.java
@@ -4,19 +4,35 @@ import java.util.Scanner;
 public class App {
 
     public static ArrayList<Object> listOfItems = new ArrayList<>();
+    public static boolean running = true;
+
+    public static void main(String[] args) throws Exception {
+        while (running ==true) {
+            chooseProduct();
+
+        }
+
+    }
+
+    public static void printList() {
+        System.out.println("\nDet du har i varukorgen är:");
+        for (Object item : listOfItems) {
+            System.out.println(item);
+        }
+        System.out.println("\n--------------------------\n");
+
+    }
 
     public static void chooseProduct() {
 
-    
         String choice = "";
         Scanner scanner = new Scanner(System.in);
 
         // Förklarar visuellt vad det handlar om + Vilka val man får
         System.out.println(
                 "Hej! Välkommen till Multifabriken! Välj vad du vill beställa!\n" + "--------------------------");
-        System.out.print(
-                "1. Bilar \n2. Godis \n3. Rör \n4. Havremjölk\n5. Kolla varukorgen\n" + "--------------------------"
-                        + "\nSkriv in vilken och klicka sedan enter: ");
+        System.out.print("1. Bilar \n2. Godis \n3. Rör \n4. Havremjölk\n5. Kolla varukorgen\n6. Avsluta \n"
+                + "--------------------------" + "\nSkriv in vilken och klicka sedan enter: ");
         choice = scanner.nextLine();
 
         // Switch statment, så man kan välja vilken man vill köpa
@@ -32,21 +48,23 @@ public class App {
                 listOfItems.add(newCandy);
                 break;
             case "3":
-                // Skapar ett objekt från klassen
                 Pipe newPipe = Pipe.newPipe();
-                // Lägger till nya bilen i inköpslistan
                 listOfItems.add(newPipe);
                 break;
 
             case "4":
-                // Skapar ett objekt från klassen
                 Oatmilk newOatmilk = Oatmilk.newOatmilk();
-                // Lägger till nya bilen i inköpslistan
                 listOfItems.add(newOatmilk);
                 break;
 
             case "5":
+                // Kör funktionen printlist, som skriver ut varje objekt ur listan
                 printList();
+                break;
+
+            case "6":
+                // Kör funktionen printlist, som skriver ut varje objekt ur listan
+                running = false;
                 break;
             default:
                 System.out.println("Ogiltigt svar, försök igen.");
@@ -55,21 +73,4 @@ public class App {
 
     }
 
-    public static void printList() {
-        System.out.println("\nDet du har i varukorgen är:");
-        for (Object item : listOfItems) {
-            System.out.println(item);
-        }
-        System.out.println("\n--------------------------\n");
-
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        while (true) {
-            chooseProduct();
-
-        }
-
-    }
 }
