8fcaa9988669bafc63f510799eea261a566cae6c
Mrsluggan <ericflyger@gmail.com>
2023-11-13 17:46:23 +0100

    Candy class
    Candy class complete

---
+++ Multifabriken/src/Candy.java
@@ -0,0 +1,17 @@
+public class Candy {
+    
+    private String taste;
+    private int amount;
+
+    public Candy(String taste, int amount) {
+
+        this.taste = taste;
+        this.amount = amount;
+
+    }
+
+
+
+
+
+}
